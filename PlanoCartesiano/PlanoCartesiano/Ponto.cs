56b7d2d747044eabc0195eeb3f02b947d364354d
Carl11br <carl11br@gmail.com>
2016-06-28 15:51:58 -0300

    Aula do dia 28/06 Plano Cartesiano

---
+++ PlanoCartesiano/PlanoCartesiano/Ponto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanoCartesiano
+{
+    class Ponto
+    {
+        public double X {get; set; }
+        public double Y { get; set; }
+        public Ponto(double X, double Y) 
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+
+        public static double Distancia(Ponto p1, Ponto p2)
+        {
+           return Math.Sqrt(Math.Pow((p1.X - p2.X), 2) + (Math.Pow((p1.Y - p2.Y), 2)));
+
+        }
+
+    }
+}
---
+++ PlanoCartesiano/PlanoCartesiano/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanoCartesiano
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Ponto> pontos = new List<Ponto>();
+            double sx = 0, sy = 0;
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Ponto p = new Ponto(double.Parse(Console.ReadLine()),double.Parse(Console.ReadLine()));
+                pontos.Add(p);
+                sx += pontos[i].X;
+                sy += pontos[i].Y;
+            }
+
+            Ponto pontomedio = new Ponto(sx / n, sy / n);
+            Console.WriteLine("Ponto médio: {0} e {1}",pontomedio.X,pontomedio.Y);
+            double perimetro = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (i == n -1)
+                {
+                  Console.WriteLine(perimetro += Ponto.Distancia(pontos[i], pontos[0]));
+                }
+               else Console.WriteLine(perimetro += Ponto.Distancia(pontos[i], pontos[i + 1]));
+
+            }
+            Console.WriteLine(perimetro);
+           
+        }
+    }
+}
